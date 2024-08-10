6261ba2c7d76d7101fa3ce92dbbffe3175c54e26
liloviy_ <chaccess@gmail.com>
2024-08-10 15:25:49 +0300

    коммит для нового мр

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,6 +11,7 @@ import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 
 const marginFromEnd = 10;
 
+//для коммита
 function App() {
   const { loading: isLoading } = useAppSelector((state) => state.ingredients);
   const disp = useAppDispatch();
